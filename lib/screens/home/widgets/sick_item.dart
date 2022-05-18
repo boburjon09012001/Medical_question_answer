3ae14c3a1537951416ab39069a718db4539f65ec
boburjon09012001 <boburislomjonov7@gmail.com>
2022-05-18 22:49:51 +0500

    added media query

--- lib/screens/home/widgets/sick_item.dart
+++ lib/screens/home/widgets/sick_item.dart
@@ -8,40 +8,43 @@ class SickItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: (){
-        Navigator.of(context).push(MaterialPageRoute(builder:
-        (context)=> DetailsPage(sick)));
-      },
-      child: Card(
-      elevation: 4,
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-          height: 100,
-         child:Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
-             child:
-             Row(
-                 children: [
-                   Flexible(
-                     child:  Text(sick.question ?? "...",
-                       maxLines: 25,
-                       softWrap: true,
-                       style:const TextStyle(
-                           fontSize: 20.0,
-                         letterSpacing: 0.5
-                       ),overflow: TextOverflow.fade,
-                     ),
-                   ),
-                 const  SizedBox(width: 20,),
-                   const Icon(Icons.chevron_right_sharp,
-                     size: 25,
-                     color: Colors.black38,
-                   ),
-                 ]
-             )
+    return  Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: InkWell(
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(builder:
+              (context)=> DetailsPage(sick)));
+        },
+        child: Card(
+          elevation: 4,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
+            height: 100,
+            child:Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+                child:
+                Row(
+                    children: [
+                      Flexible(
+                        child:  Text(sick.question ?? "...",
+                          maxLines: 25,
+                          softWrap: true,
+                          style:const TextStyle(
+                              fontSize: 20.0,
+                              letterSpacing: 0.5
+                          ),overflow: TextOverflow.fade,
+                        ),
+                      ),
+                      const  SizedBox(width: 20,),
+                      const Icon(Icons.chevron_right_sharp,
+                        size: 25,
+                        color: Colors.black38,
+                      ),
+                    ]
+                )
 
-         ),
+            ),
+          ),
         ),
       ),
     );
